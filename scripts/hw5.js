11e1037e8a6aa5db9d1538a1dd3d27257d5e0c31
IgorPv118 <pavlov18101999@gmail.com>
2023-08-02 14:22:37 +0600

    feat: added hw5

---
+++ scripts/hw5.js
@@ -0,0 +1,35 @@
+"use strict";
+
+/* const teachersName = "Dima"
+const myData = {
+    name : "Igor",
+    surname : "Pavlov",
+    age : 23,
+    test : null,
+    sayHello : () => {
+        return `Hello ${teachersName}`
+    },
+    printAge : function(){
+        return `Мне ${this.age} года`
+    },
+    printData : function(){
+        let res = " "
+        for(let i = 0; i < Object.keys(myData).length; i++){
+            res = res +(`${Object.keys(myData)[i]} : ${Object.values(myData)[i]}\n`)
+        }
+        return res
+    },
+    objectCheck : function(){
+        let objectPresence = false
+        for(let k = 0; k < Object.values(myData).length; k++){
+            if(typeof(Object.values(myData)[k]) === "object" && Object.values(myData)[k] !== null){
+                objectPresence = true
+                break
+            }
+        }
+        return objectPresence
+    }
+}
+myData.pet = "Cat"
+myData.profession = "Miner"
+console.log(myData.objectCheck()) */
